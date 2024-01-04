88c494fe1fed40f45a8e941f98fa12ae47c7d5f0
nfjsg <dasarathan@icloud.com>
2024-01-03 21:03:27 -0500

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// models/index.js:
+const SocialUser = require('./SocialUser');
+const UserThought = require('./UserThought');
+const UserReaction = require('./UserReaction');
+
+module.exports = { SocialUser, UserThought, UserReaction };
