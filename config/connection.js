592d814f76c0eba7cbb58e8f4e288ecb98420b33
nfjsg <dasarathan@icloud.com>
2024-01-07 21:02:01 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/socialNetworkDB')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 module.exports = mongoose.connection;
