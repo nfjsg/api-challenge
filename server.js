be0f0ef792fe3646f04e5ec6bc60282df53e8299
nfjsg <dasarathan@icloud.com>
2024-01-07 21:00:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const databaseConnection = require('./config/connection');
+const apiRoutes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(routes);
+app.use(apiRoutes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+databaseConnection.once('open', () => {
+  app.listen(PORT, () => console.log(`Server is now live on port ${PORT}`));
 });
